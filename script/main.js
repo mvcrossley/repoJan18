7aaf73b3bc09488c061060b58d3877ec0a4f3899
Crossley <m_crossley@tss-hr420-dm02.fc.ca>
2017-01-18 13:36:01 -0500

    our first jquery lesson

--- script/main.js
+++ script/main.js
@@ -1,31 +1,53 @@
 
 (function(){
-	var theImages = document.querySelectorAll(".image-holder");
-	var heading = document.querySelector(".heading");
-	var title = document.querySelector(".main-copy h2");
-	var paragraph = document.querySelector(".main-copy p");
-	var appliedClass;
+	// var theImages = document.querySelectorAll(".image-holder"),
+	// 	heading = document.querySelector(".heading"),
+	// 	title = document.querySelector(".main-copy h2"),
+	// 	paragraph = document.querySelector(".main-copy p"),
+	// 	appliedClass;
+
+	var $theImages = $(".image-holder"),
+		$heading = $(".heading"),
+		$title = $(".main-copy h2"),
+		$paragraph = $(".main-copy p"),
+		appliedClass;
+
 
 	function changeElements()
 	{
-		heading.classList.remove(appliedClass); //Clears any existing classes so that it may change back each time
-		title.classList.remove(appliedClass);
+		//heading.classList.remove(appliedClass); //Clears any existing classes so that it may change back each time
+		$heading.removeClass(appliedClass); //This is the jquery method for that same function
+		//title.classList.remove(appliedClass);
+		$title.removeClass(appliedClass);
 
-		heading.classList.add(this.id);
-		title.classList.add(this.id);
-		
-		appliedClass = this.id;
-		title.firstChild.nodeValue = dynamicContent[this.id].headline; //These are accessing parts of an object located in contentObject
-		paragraph.firstChild.nodeValue = dynamicContent[this.id].text;
+		//appliedClass = this.id;
+		appliedClass = event.currentTarget.id; //There is no "this.id" in jquery
+
+		//heading.classList.add(this.id);
+		$heading.addClass(event.currentTarget.id);
+		//title.classList.add(this.id);
+		$title.addClass(event.currentTarget.id);
+
+		//title.firstChild.nodeValue = dynamicContent[this.id].headline; //These are accessing parts of an object located in contentObject
+		$title.text(dynamicContent[event.currentTarget.id].headline);
+		//paragraph.firstChild.nodeValue = dynamicContent[this.id].text;
+		$paragraph.text(dynamicContent[event.currentTarget.id].text);
 	}
 
-	[].forEach.call(theImages, function(image) {
-		image.addEventListener('click', changeElements, true);
+	$theImages.click(function(){
+		//console.log("Clicked!");
+		changeElements();
 	});
 
-	title.firstChild.nodeValue = dynamicContent["spring"].headline; //These change the content on load
-	paragraph.firstChild.nodeValue = dynamicContent["spring"].text;
-	heading.classList.add("spring");
+	// [].forEach.call(theImages, function(image) {
+	// 	image.addEventListener('click', changeElements, true);
+	// });
 
+	//title.firstChild.nodeValue = dynamicContent["spring"].headline; //These change the content on load
+	$title.text(dynamicContent["spring"].headline);
+	//paragraph.firstChild.nodeValue = dynamicContent["spring"].text;
+	$paragraph.text(dynamicContent["spring"].text);
+	//heading.classList.add("spring");
+	$heading.addClass("spring");
 	
 })();
